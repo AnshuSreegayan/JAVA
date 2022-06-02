191daca6a5e6ceb15ceb5184d1d975a789f44e3f
Anshu Kumar <98792386+AnshuSreegayan@users.noreply.github.com>
2022-06-02 21:03:58 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,27 @@
+class A 
+{
+	int c=9;
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
+class B
+{
+	int f=65;
+	public static void main(String [] q)
+	{
+		System.out.println("from class B");
+
+	}
+
+	}
+class C
+{
+	int y=80;
+	public static void main(String[] s)
+	{
+		System.out.println("from C");
+
+	}
+}
